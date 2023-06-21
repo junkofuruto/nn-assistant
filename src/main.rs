c19cce9308afee8c13f7a4189ee8756849b47d4e
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-06-21 08:18:35 +0300

    dropped idea of making web interface for this

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-use yew::prelude::*;
-
-
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-      
-    }
-}
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+      println!("gay sex");
 }
