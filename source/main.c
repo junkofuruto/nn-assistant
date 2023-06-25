7a76bf77e89cff7432250e076b10a4564b79b9d4
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-06-25 21:06:42 +0300

    s

--- source/main.c
+++ source/main.c
@@ -20,15 +20,14 @@ nf_model nf_model_alloc(void) {
     return m;
 }
 
-float forward(nf_model m) {
+void nf_forward(nf_model m) {
     nf_mat_dot(m.l1_a, m.l0_a, m.l1_w);
     nf_mat_sum(m.l1_a, m.l1_b);
     nf_mat_apply_sigmoid(m.l1_a);
     
     nf_mat_dot(m.l2_a, m.l1_a, m.l2_w);
     nf_mat_sum(m.l2_a, m.l2_b);
-    nf_mat_apply_sigmoid(m.l2_a); 
-    return m.l2_a.data[0];    
+    nf_mat_apply_sigmoid(m.l2_a);
 }
 
 float cost(nf_model m, nf_matrix ti, nf_matrix to) {
@@ -40,7 +39,7 @@ float cost(nf_model m, nf_matrix ti, nf_matrix to) {
         nf_matrix x = nf_mat_row(ti, i);
         nf_matrix y = nf_mat_row(to, i);
         nf_mat_copy(m.l0_a, x);
-        forward(m);
+        nf_forward(m);
         size_t q = to.cols;
         for (size_t j = 0; j < q; ++j) {
             float d = NF_MAT_AT(m.l2_a, 0, j) - NF_MAT_AT(y, 0, j);
@@ -57,12 +56,12 @@ float data[] = {
     1, 1, 0,
 };
 
-void finite_diff(nf_model m, nf_model g, float eps, nf_matrix ti, nf_matrix to) {
+void nf_finite_diff(nf_model m, nf_model g, float eps, nf_matrix ti, nf_matrix to) {
     float saved;
     float c = cost(m, ti, to);
 
-    for (size_t i; i < m.l1_w.rows; ++i) {
-        for (size_t j; j < m.l1_w.cols; ++j) {
+    for (size_t i = 0; i < m.l1_w.rows; ++i) {
+        for (size_t j = 0; j < m.l1_w.cols; ++j) {
             saved = NF_MAT_AT(m.l1_w, i, j);
             NF_MAT_AT(m.l1_w, i, j) += eps;
             NF_MAT_AT(g.l1_w, i, j) = (cost(m, ti, to) - c) / eps;
@@ -70,8 +69,8 @@ void finite_diff(nf_model m, nf_model g, float eps, nf_matrix ti, nf_matrix to) 
         }
     }
 
-    for (size_t i; i < m.l1_b.rows; ++i) {
-        for (size_t j; j < m.l1_b.cols; ++j) {
+    for (size_t i = 0; i < m.l1_b.rows; ++i) {
+        for (size_t j = 0; j < m.l1_b.cols; ++j) {
             saved = NF_MAT_AT(m.l1_b, i, j);
             NF_MAT_AT(m.l1_b, i, j) += eps;
             NF_MAT_AT(g.l1_b, i, j) = (cost(m, ti, to) - c) / eps;
@@ -79,8 +78,8 @@ void finite_diff(nf_model m, nf_model g, float eps, nf_matrix ti, nf_matrix to) 
         }
     }
     
-    for (size_t i; i < m.l2_w.rows; ++i) {
-        for (size_t j; j < m.l2_w.cols; ++j) {
+    for (size_t i = 0; i < m.l2_w.rows; ++i) {
+        for (size_t j = 0; j < m.l2_w.cols; ++j) {
             saved = NF_MAT_AT(m.l2_w, i, j);
             NF_MAT_AT(m.l2_w, i, j) += eps;
             NF_MAT_AT(g.l2_w, i, j) = (cost(m, ti, to) - c) / eps;
@@ -88,8 +87,8 @@ void finite_diff(nf_model m, nf_model g, float eps, nf_matrix ti, nf_matrix to) 
         }
     }
 
-    for (size_t i; i < m.l2_b.rows; ++i) {
-        for (size_t j; j < m.l2_b.cols; ++j) {
+    for (size_t i = 0; i < m.l2_b.rows; ++i) {
+        for (size_t j = 0; j < m.l2_b.cols; ++j) {
             saved = NF_MAT_AT(m.l2_b, i, j);
             NF_MAT_AT(m.l2_b, i, j) += eps;
             NF_MAT_AT(g.l2_b, i, j) = (cost(m, ti, to) - c) / eps;
@@ -98,8 +97,30 @@ void finite_diff(nf_model m, nf_model g, float eps, nf_matrix ti, nf_matrix to) 
     }
 }
 
-void learn(nf_model m, nf_model g) {
+void nf_learn(nf_model m, nf_model g, float rate) {
+    for (size_t i; i < m.l1_w.rows; ++i) {
+        for (size_t j; j < m.l1_w.cols; ++j) {
+            NF_MAT_AT(m.l1_w, i, j) -= rate * NF_MAT_AT(g.l1_w, i, j);
+        }
+    }
+
+    for (size_t i; i < m.l1_b.rows; ++i) {
+        for (size_t j; j < m.l1_b.cols; ++j) {
+            NF_MAT_AT(m.l1_b, i, j) -= rate * NF_MAT_AT(g.l1_b, i, j);
+        }
+    }
     
+    for (size_t i; i < m.l2_w.rows; ++i) {
+        for (size_t j; j < m.l2_w.cols; ++j) {
+            NF_MAT_AT(m.l2_w, i, j) -= rate * NF_MAT_AT(g.l2_w, i, j);
+        }
+    }
+
+    for (size_t i; i < m.l2_b.rows; ++i) {
+        for (size_t j; j < m.l2_b.cols; ++j) {
+            NF_MAT_AT(m.l2_b, i, j) -= rate * NF_MAT_AT(g.l2_b, i, j);
+        }
+    }
 }
 
 int main(void) {
@@ -123,24 +144,16 @@ int main(void) {
 
     nf_model m = nf_model_alloc();
     nf_model g = nf_model_alloc();
-    nf_mat_rand(m.l1_w, -1.0f, 1.0f);
-    nf_mat_rand(m.l1_b, -5.0f, 5.0f);
-    nf_mat_rand(m.l2_w, -1.0f, 1.0f);
-    nf_mat_rand(m.l2_b, -5.0f, 5.0f);
-
-    printf("Cost: %f\n", cost(m, ti, to));
-    finite_diff(m, g, 1e-1f, ti, to);
-
-    return 0;
-
-    for (size_t i = 0; i < 2; ++i) {
-        for (size_t j = 0; j < 2; ++j) {
-            NF_MAT_AT(m.l0_a, 0, 0) = i;
-            NF_MAT_AT(m.l0_a, 0, 1) = j;
-            printf("%zu ^ %zu = %f\n", i, j, forward(m));
-        }
-    }
-    
-
+    nf_mat_rand(m.l1_w, 0, 1);
+    nf_mat_rand(m.l1_b, 0, 1);
+    nf_mat_rand(m.l2_w, 0, 1);
+    nf_mat_rand(m.l2_b, 0, 1);
+    float c_b = cost(m, ti, to);
+    printf("| COST_B: %f\n", c_b);
+    nf_finite_diff(m, g, -1, ti, to);
+    nf_learn(m, g, 0.1);
+    float c_a = cost(m, ti, to);
+    printf("| COST_A: %f\n", c_a);
+    printf("| COST_DIFF: %f\n", c_a - c_b);
     return 0;
 }
