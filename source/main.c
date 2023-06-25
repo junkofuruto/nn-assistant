295957e223bb7152692299e08c24be6322919616
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-06-25 04:36:02 +0300

    port

--- source/main.c
+++ source/main.c
@@ -5,19 +5,20 @@
 int main(void) {
     nf_init();
 
-    nf_matrix mat1 = nf_mat_alloc(2, 3);
-    nf_matrix mat2 = nf_mat_alloc(2, 3);
-    
+    nf_matrix mat1 = nf_mat_alloc(1, 2);
     nf_mat_rand(mat1, -1.0f, 1.0f);
+
+    nf_matrix mat2 = nf_mat_alloc(2, 2);
     nf_mat_rand(mat2, -1.0f, 1.0f);
 
-    nf_mat_print(mat1);
-    puts("+");
-    nf_mat_print(mat2);
-    puts("------------------------------");
-    nf_mat_sum(mat1, mat2);
-    nf_mat_print(mat1);
+    nf_matrix mat_dest = nf_mat_alloc(1, 2);
 
+    nf_mat_dot(mat_dest, mat1, mat2);
+    nf_mat_print(mat1);
+    puts("*");
+    nf_mat_print(mat2);
+    puts("---------------------");
+    nf_mat_print(mat_dest);
 
 
     return 0;
