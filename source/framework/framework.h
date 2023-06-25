12a3e156cc9e4a6118c0111f12a0c11b75f14dff
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-06-25 04:30:37 +0300

    s

--- source/framework/framework.h
+++ source/framework/framework.h
@@ -6,6 +6,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 
 #define NF_MAT_AT(m, i, j) (m).data[(i) * (m).cols + (j)]
 
@@ -17,13 +18,20 @@ typedef struct {
 
 nf_matrix nf_mat_alloc(size_t rows, size_t cols);
 void nf_mat_dot(nf_matrix dest, nf_matrix m1, nf_matrix m2);
-void nf_mat_sum(nf_matrix dest, nf_matrix m1, nf_matrix m2);
+void nf_mat_rand(nf_matrix m, float min, float max);
+void nf_mat_fill(nf_matrix dest, float value);
+void nf_mat_sum(nf_matrix dest, nf_matrix m);
 void nf_mat_print(nf_matrix m);
+float nf_rand_float();
+void nf_init();
 
 #endif
 
 #ifdef __NFRAMEWORK_IMPL__
 
+void nf_init() {
+    srand(time(0));
+}
 nf_matrix nf_mat_alloc(size_t rows, size_t cols) {
     nf_matrix mat;
     mat.rows = rows;
@@ -33,25 +41,54 @@ nf_matrix nf_mat_alloc(size_t rows, size_t cols) {
     memset(mat.data, 0.0f, sizeof(float) * rows * cols);
     return mat;
 }
-void nf_mat_rand(nf_matrix m) {
-    
+float nf_rand_float() {
+    return (float) rand() / (float) RAND_MAX;
+}
+void nf_mat_rand(nf_matrix m, float min, float max) {
+    for (size_t i = 0; i < m.rows; ++i) {
+        for (size_t j = 0; j < m.cols; ++j) {
+            NF_MAT_AT(m, i, j) = nf_rand_float() * (max - min) + min;
+        }
+    }
+}
+void nf_mat_fill(nf_matrix dest, float value) {
+    for (size_t i = 0; i < dest.rows; ++i) {
+        for (size_t j = 0; j < dest.cols; ++j) {
+            NF_MAT_AT(dest, i, j) = value;
+        }
+    }
 }
 void nf_mat_dot(nf_matrix dest, nf_matrix m1, nf_matrix m2) {
-    (void) dest;
-    (void) m1;
-    (void) m2;
+    assert(m1.cols == m2.rows);
+    assert(dest.rows == m1.rows);
+    assert(dest.cols == m2.cols);
+
+    size_t n = m1.cols;
+
+    for (size_t i = 0; i < dest.rows; ++i) {
+        for (size_t j = 0; j < dest.cols; ++j) {
+            for (size_t k = 0; k < n; ++k) {
+                NF_MAT_AT(dest, i, j) += NF_MAT_AT(m1, i, k) * NF_MAT_AT(m2, k, j);
+            }
+        }
+    }
+
 }
-void nf_mat_sum(nf_matrix dest, nf_matrix m1, nf_matrix m2) {
-    (void) dest;
-    (void) m1;
-    (void) m2;
+void nf_mat_sum(nf_matrix dest, nf_matrix m) {
+    assert(dest.rows == m.rows);
+    assert(dest.cols == m.cols);
+    for (size_t i = 0; i < dest.rows; ++i) {
+        for (size_t j = 0; j < dest.cols; ++j) {
+            NF_MAT_AT(dest, i, j) += NF_MAT_AT(m, i, j);
+        }
+    }
 }
 void nf_mat_print(nf_matrix m) {
     for (size_t i = 0; i < m.rows; ++i) {
         for (size_t j = 0; j < m.cols; ++j) {
-            printf("%f ", NF_MAT_AT(m, i, j));
+            printf("%9.6f  ", NF_MAT_AT(m, i, j));
         }
-        printf("\n");
+        puts("");
     }
 }
 
